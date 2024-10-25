82473ac14593218f5c001827918057e815d3e11f
Ben Demostudent40 <demostudent40@firstdraft.com>
2024-10-25 15:05:24 +0000

    .fetch fallback on nil for rails db login

--- config/initializers/rails_db.rb
+++ config/initializers/rails_db.rb
@@ -1,9 +1,9 @@
 if Object.const_defined?('RailsDb')
   RailsDb.setup do |config|
-    # # enabled or not
+    # enabled or not
     # config.enabled = Rails.env.development?
 
-    # # automatic engine routes mounting
+    # automatic engine routes mounting
     # config.automatic_routes_mount = true
 
     # set tables which you want to hide ONLY
@@ -12,19 +12,19 @@ if Object.const_defined?('RailsDb')
     # set tables which you want to show ONLY
     # config.white_list_tables = ['posts', 'comments']
 
-    # # Enable http basic authentication
+    # Enable http basic authentication
     config.http_basic_authentication_enabled = true
 
-    # # Enable http basic authentication
-    config.http_basic_authentication_user_name = ENV.fetch("RAILS_DB_NAME")
+    # Enable http basic authentication
+    config.http_basic_authentication_user_name = ENV.fetch("RAILS_DB_USERNAME", nil)
 
-    # # Enable http basic authentication
-    config.http_basic_authentication_password = ENV.fetch("RAILS_DB_PASSWORD")
+    # Enable http basic authentication
+    config.http_basic_authentication_password = ENV.fetch("RAILS_DB_PASSWORD", nil)
 
-    # # Enable http basic authentication
+    # Enable http basic authentication
     # config.verify_access_proc = proc { |controller| true }
 
-    # # Sandbox mode (only read-only operations)
+    # Sandbox mode (only read-only operations)
     # config.sandbox = false
   end
 end
