1f113823d089cb33935b227e8adf28f577bb4000
Ben Demostudent40 <demostudent40@firstdraft.com>
2024-10-25 18:08:35 +0000

    wrap entire config in env check

--- config/initializers/rails_db.rb
+++ config/initializers/rails_db.rb
@@ -8,31 +8,22 @@ if Object.const_defined?('RailsDb')
 
       # automatic engine routes mounting
       config.automatic_routes_mount = true
+
+      # set tables which you want to hide ONLY
+      config.black_list_tables = ["ar_internal_metadata"]
+
+      # Enable http basic authentication
+      config.http_basic_authentication_enabled = true
+
+      # Enable http basic authentication
+      config.http_basic_authentication_user_name = ENV.fetch("RAILS_DB_USERNAME", nil)
+
+      # Enable http basic authentication
+      config.http_basic_authentication_password = ENV.fetch("RAILS_DB_PASSWORD", nil)
     else
       puts("\n****\nRAILSDB DISABLED\n*****\n")
       config.enabled = false
       config.automatic_routes_mount = false
     end
-
-    # set tables which you want to hide ONLY
-    config.black_list_tables = ["ar_internal_metadata"]
-
-    # set tables which you want to show ONLY
-    # config.white_list_tables = ['posts', 'comments']
-
-    # Enable http basic authentication
-    config.http_basic_authentication_enabled = true
-
-    # Enable http basic authentication
-    config.http_basic_authentication_user_name = ENV.fetch("RAILS_DB_USERNAME", nil)
-
-    # Enable http basic authentication
-    config.http_basic_authentication_password = ENV.fetch("RAILS_DB_PASSWORD", nil)
-
-    # Enable http basic authentication
-    # config.verify_access_proc = proc { |controller| true }
-
-    # Sandbox mode (only read-only operations)
-    # config.sandbox = false
   end
 end
